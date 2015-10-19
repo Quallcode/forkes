94bbfc9e6d0aed35b8623c28970c313e465bdbc2
adejulianto <ade.shinz@gmail.com>
2015-10-19 21:30:10 +0700

    new menu dashboard

--- application/logs/log-2015-10-19.php
+++ application/logs/log-2015-10-19.php
@@ -1,3 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2015-10-19 15:34:48 --> 404 Page Not Found: Verify/index
+ERROR - 2015-10-19 16:13:18 --> Severity: Notice --> Undefined variable: SESSION C:\xampp\htdocs\forkes\application\views\header.php 21
+ERROR - 2015-10-19 16:13:37 --> Severity: Notice --> Undefined variable: session C:\xampp\htdocs\forkes\application\views\header.php 21
+ERROR - 2015-10-19 16:14:17 --> Severity: Notice --> Undefined variable: session C:\xampp\htdocs\forkes\application\views\header.php 21
+ERROR - 2015-10-19 16:14:45 --> Severity: Notice --> Undefined variable: session C:\xampp\htdocs\forkes\application\views\header.php 21
+ERROR - 2015-10-19 16:16:23 --> Severity: Parsing Error --> syntax error, unexpected 'echo' (T_ECHO) C:\xampp\htdocs\forkes\application\views\header.php 21
+ERROR - 2015-10-19 16:16:26 --> Severity: Parsing Error --> syntax error, unexpected 'echo' (T_ECHO) C:\xampp\htdocs\forkes\application\views\header.php 21
+ERROR - 2015-10-19 16:16:36 --> Severity: Notice --> Array to string conversion C:\xampp\htdocs\forkes\application\views\header.php 21
