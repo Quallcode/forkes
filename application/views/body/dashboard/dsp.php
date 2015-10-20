af4860b40318f860d833e593399bc695677c8d27
adejulianto <ade.shinz@gmail.com>
2015-10-20 21:03:21 +0700

    Data table in ATC menu

--- application/views/body/dashboard/dsp.php
+++ application/views/body/dashboard/dsp.php
@@ -231,8 +231,11 @@
                                         <div class="row">
                                           <div class="col-xs-12">
                                             <div class="box">
+                                              <div class="box-header">
+                                                <h3 class="box-title">Data Table With Full Features</h3>
+                                              </div><!-- /.box-header -->
                                               <div class="box-body">
-                                                <table id="example2" class="table table-bordered table-hover">
+                                                <table id="example1" class="table table-bordered table-striped">
                                                   <thead>
                                                     <tr>
                                                       <th>Product Name</th>
--- application/views/wrapper.php
+++ application/views/wrapper.php
@@ -29,6 +29,8 @@
     <link rel="stylesheet" href="<?=base_url()?>includes/css_dashboard/plugins/daterangepicker/daterangepicker-bs3.css">
     <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="<?=base_url()?>includes/css_dashboard/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="<?=base_url()?>includes/css_dashboard/plugins/datatables/dataTables.bootstrap.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -246,9 +248,26 @@
     <script src="<?=base_url()?>includes/css_dashboard/plugins/fastclick/fastclick.min.js"></script>
     <!-- AdminLTE App -->
     <script src="<?=base_url()?>includes/css_dashboard/dist/js/app.min.js"></script>
+    <!-- DataTables -->
+    <script src="<?=base_url()?>includes/css_dashboard/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?=base_url()?>includes/css_dashboard/plugins/datatables/dataTables.bootstrap.min.js"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <!--<script src="<?=base_url()?>includes/css_dashboard/dist/js/pages/dashboard.js"></script>-->
     <!-- AdminLTE for demo purposes -->
     <script src="<?=base_url()?>includes/css_dashboard/dist/js/demo.js"></script>
+
+    <script>
+      $(function () {
+        $("#example1").DataTable();
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false
+        });
+      });
+    </script>
   </body>
 </html>
