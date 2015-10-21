cd0f1a3749ed5c3b4039e1bd7bdba0353062dcb3
Julius Cesario <lixus.julius17@gmail.com>
2015-10-21 18:34:18 +0700

    Model Database
    Model ENCHANCE for SEDIAAN

--- application/controllers/Sediaan.php
+++ application/controllers/Sediaan.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sediaan extends CI_Controller {
-  //CONSTRUCT FOR LOGIN
+  //CONSTRUCT FOR SEDIAAN
   function __construct(){
     parent::__construct();
-    //INIT MODEL USERS
+    //INIT MODEL TRANSACTION
     $this->load->model('Model_Transaction');
     //CHECK SESSION
     $sess = $this->session->userdata('user_data');
@@ -13,13 +13,17 @@ class Sediaan extends CI_Controller {
       redirect('login','refresh');
       exit;
     }
+    //SET TABLE NAME
+    define('TABLE','sediaan')
     //SET BREADCRUMB
     $this->session->set_userdata(array('breadcrumb'=>'sediaan'));
   }
 
   //INDEX FOR FIRST VIEW
 	public function Index(){
-
+    //GET SEDIAAN DATA
+    $sediaan = $this->Model_Get_Sediaan->Normal_Select(TABLE);
+    print_r($sediaan);exit;
     //DECLARE VIEW DATA FOR WRAPPER
     $view_data['body']   = 'body/master/sediaan/record_dsp';
     //LOAD VIEW DATA TO WRAPPER
@@ -27,7 +31,7 @@ class Sediaan extends CI_Controller {
 	}
   //END OF INDEX FOR FIRST VIEW
 
-  //POST LOGIN
+  //POST SEDIAAN
   public function Insert(){
     //GET POST DATA
     $post = $this->input->post();
@@ -38,18 +42,11 @@ class Sediaan extends CI_Controller {
       //LOAD VIEW DATA TO WRAPPER
       $this->load->view('wrapper',$view_data);
     }else{
-      //GET DATA POST
+      //INSERT TO DATABASE
+      $this->Model_Transaction->Insert_To_Db($post,TABLE);
+      redirect('sediaan');
     }
   }
-  //END OF POST LOGIN
-
-  //LOGOUT FUNCTION
-  public function Logout(){
-      $this->session->unset_userdata('user_data');
-      session_write_close();
-      redirect('login','refresh');
-      exit;
-  }
-  //END OF LOGOUT
+  //END OF POST SEDIAAN
 }
 ?>
--- application/models/Model_Transaction.php
+++ application/models/Model_Transaction.php
@@ -1,21 +1,11 @@
 <?php
 class Model_Transaction extends CI_Model {
-	function Check_Login_Valid ($post) {
-		$this->db->select('*');
-		$this->db->from('users');
-		$this->db->where('users.username = ',$post['username']);
-		$this->db->where('users.password =',md5($post['password']));
-
-		$result = $this->db->get();
-		if ($result->num_rows() == 1) {
-			foreach($result->result_array() as $row){
-				$data = $row;
-			}
-			return $data;
-		}
-		else{
-			return FALSE;
-		}
+	function Insert_To_Db ($data,$table) {
+		$this->db->insert($table, $data);
+	}
+	function Update_To_Db ($data,$table,$param,$on){
+		$this->db->where($param, $on);
+		$this->db->update($table, $data);
 	}
 }
 ?>
