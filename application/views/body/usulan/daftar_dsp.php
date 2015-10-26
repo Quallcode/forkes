527eacb1f6609d088608010af650e122a54ee6df
Ricko Noviandi Karya <rickonoviandikarya@gmail.com>
2015-10-26 21:08:01 +0700

    Usulan
    View and Controller

---
+++ application/views/body/usulan/daftar_dsp.php
@@ -0,0 +1,59 @@
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      DAFTAR USULANa
+    </h1>
+  </section>
+  <section class="content">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">Data Table With Full Features</h3>
+          </div><!-- /.box-header -->
+          <div class="box-body">
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Rendering engine</th>
+                  <th>Browser</th>
+                  <th>Platform(s)</th>
+                  <th>Engine version</th>
+                  <th>CSS grade</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet
+                    Explorer 4.0</td>
+                  <td>Win 95+</td>
+                  <td> 4</td>
+                  <td>X</td>
+                </tr>
+              </tbody>
+              <tfoot>
+              </tfoot>
+            </table>
+          </div><!-- /.box-body -->
+        </div><!-- /.box -->
+      </div><!-- /.col -->
+    </div>   <!-- /.row -->
+  </section>
+</div>
+
+  <!-- page script -->
+    <script>
+      $(function () {
+        $("#example1").DataTable();
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false
+        });
+      });
+    </script>
---
+++ application/views/body/usulan/verifikasi_dsp.php
@@ -0,0 +1,212 @@
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      VERIFIKASI USULANa
+    </h1>
+  </section>
+  <section class="content">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- general form elements -->
+        <div class="box box-info">
+          <table style="width:50%">
+            <tr>
+              <td>Nama Rumah Sakit</td>
+              <td>:</td>
+              <td>Rumah Sakit ABC</td>
+            </tr>
+            <tr>
+              <td>Kepemilikan</td>
+              <td>:</td>
+              <td>Rumah Sakit ABC</td>
+            </tr>
+            <tr>
+              <td>Jenis</td>
+              <td>:</td>
+              <td>Rumah Sakit ABC</td>
+            </tr>
+            <tr>
+              <td>Tipe</td>
+              <td>:</td>
+              <td>Rumah Sakit ABC</td>
+            </tr>
+            <tr>
+              <td>Alamat</td>
+              <td>:</td>
+              <td>Rumah Sakit ABC</td>
+            </tr>
+          </table>
+        </div><!-- /.box -->
+      </div><!-- /.col -->
+    </div>   <!-- /.row -->
+
+    <div class="row">
+      <div class="col-md-12">
+        <!-- general form elements -->
+        <div class="box box-info">
+          <form class="form-horizontal">
+            <div class="box-body">
+              <div class="form-group">
+                <label for="inputNoEfornas" class="col-sm-2 control-label">No E-Fornas</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputNoEfornas" placeholder="No E-fornas">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputSurat" class="col-sm-2 control-label">Surat Pengantar</label>
+                <div class="col-sm-10">
+                  <input type="file" id="inputSurat">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputDaftarUsulan" class="col-sm-2 control-label">Daftar Usulan Obat</label>
+                <div class="col-sm-10">
+                  <input type="file" id="inputDaftarUsulan">
+                </div>
+              </div>
+            </div><!-- /.box-body -->
+            <div class="box-footer">
+              <button type="submit" class="btn btn-default">Batal</button>
+              <button type="submit" class="btn btn-info pull-right">Masukkan</button>
+            </div><!-- /.box-footer -->
+          </form>
+        </div><!-- /.box -->
+      </div><!-- /.col -->
+    </div>   <!-- /.row -->
+
+    <div class="row">
+      <div class="col-md-12">
+        <!-- general form elements -->
+        <div class="box box-info">
+          <form class="form-horizontal">
+            <div class="box-body">
+              <!-- select -->
+              <div class="form-group">
+                <label for="inputNamaObat" class="col-sm-2 control-label">Nama Obat</label>
+                <div class="col-sm-10">
+                  <select class="form-control">
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                    <option>option 4</option>
+                    <option>option 5</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputSediaan" class="col-sm-2 control-label">Sediaan</label>
+                <div class="col-sm-10">
+                  <select class="form-control">
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                    <option>option 4</option>
+                    <option>option 5</option>
+                  </select>
+                </div>
+              </div>
+              <!-- checkbox -->
+              <div class="form-group">
+                <label for="inputInfus" class="col-sm-2 control-label"></label>
+                <div class="col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox">
+                      Larutan Infus
+                    </label>
+                  </div>
+                </div>
+                <label for="inputInjeksi" class="col-sm-2 control-label"></label>
+                <div class="col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox">
+                      Larutan Injeksi
+                    </label>
+                  </div>
+                </div>
+                <label for="inputSerbukInjeksi" class="col-sm-2 control-label"></label>
+                <div class="col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox">
+                      Serbuk Injeksi
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputKekuatan" class="col-sm-2 control-label">Kekuatan</label>
+                <div class="col-sm-10">
+                  <input type="number" class="form-control" id="inputKekuatan" placeholder="Kekuatan">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputSatuan" class="col-sm-2 control-label">Satuan</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputSatuan" placeholder="Satuan">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputLink" class="col-sm-2 control-label">Link Jurnal</label>
+                <div class="col-sm-10">
+                  <input type="file" id="inputLink">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputAlasan" class="col-sm-2 control-label">Alasan</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" rows="3" placeholder="Masukkan Alasan ..."></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputRestriksi" class="col-sm-2 control-label">Restriksi baru</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" rows="3" placeholder="Masukkan Restriksi baru ..."></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputRestriksi" class="col-sm-2 control-label">Tingkat Faskes</label>
+                <div class="radio">
+                  <div class="col-sm-10">
+                    <label>
+                      <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">TK1
+                    </label>
+                    <label>
+                      <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">TK2
+                    </label>
+                    <label>
+                      <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">TK3
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputTypeUsulan" class="col-sm-2 control-label">Type Usulan</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputTypeUsulan" placeholder="Type Usulan">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPRB" class="col-sm-2 control-label">PRB</label>
+                <div class="col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox">
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div><!-- /.box-body -->
+            <div class="box-footer">
+              <button type="submit" class="btn btn-default">Batal</button>
+              <button type="submit" class="btn btn-info pull-right">Masukkan</button>
+            </div><!-- /.box-footer -->
+          </form>
+        </div><!-- /.box -->
+      </div><!-- /.col -->
+    </div>   <!-- /.row -->
+
+  </section>
+</div>
