21eb248700cba21a41f56f8fa73a41712b37aa48
adejulianto <ade.shinz@gmail.com>
2015-10-26 22:24:17 +0700

    modul pop up detail obat

--- application/views/body/usulan/daftar_tidak_lengkap_dsp.php
+++ application/views/body/usulan/daftar_tidak_lengkap_dsp.php
@@ -29,7 +29,7 @@
                   <td>Internet
                     Explorer 4.0</td>
                   <td>Win 95+</td>
-                  <td> 4</td>
+                  <td><a data-toggle="modal" href="#myModal">DETAIL</a></td>
                   <td>DITOLAK</td>
                 </tr>
               </tbody>
@@ -42,7 +42,51 @@
     </div>   <!-- /.row -->
   </section>
 </div>
-
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" style="width:75%; background:#fff">
+    <div class="model-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+        <h4 class="modal-tittle">Detail Obat</h4>
+      </div>
+      <div class="modal-body">
+        <table id="example1" class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Obat</th>
+              <th>Sediaan</th>
+              <th>Kekuatan</th>
+              <th>Satuan</th>
+              <th>Link Journal</th>
+              <th>Alasan</th>
+              <th>Restriksi</th>
+              <th>Type Usulan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>No</td>
+              <td>Nama Obat</td>
+              <td>Sediaan</td>
+              <td>Kekuatan</td>
+              <td>Satuan</td>
+              <td>Link Journal</td>
+              <td>Alasan</td>
+              <td>Restriksi</td>
+              <td>Type Usulan</td>
+            </tr>
+          </tbody>
+          <tfoot>
+          </tfoot>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
   <!-- page script -->
     <script>
       $(function () {
